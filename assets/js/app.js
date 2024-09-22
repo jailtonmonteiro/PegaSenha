dfb73178808b437fbece8d5d1cd19f652a05741a
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-09-22 14:19:05 -0300

    Desafio extra: Adicionar seletores de complexidade

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,37 @@
-document.getElementById("gerar").addEventListener("click", function(){
-    let comprimento = document.getElementById("comprimento").value;
-    let senha = gerarSenha(comprimento);
+document.getElementById("gerar").addEventListener("click", function () {
+  let comprimento = document.getElementById("comprimento").value;
+  let senha = gerarSenha(comprimento);
 
-    document.getElementById("senha").textContent = senha;
+  document.getElementById("senha").textContent = senha;
 });
 
 function gerarSenha(comprimento) {
-    let caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let senha = '';
+  let maiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let minusculas = "abcdefghijklmnopqrstuvwxyz";
+  let simbolos = "!@#$%&*";
+  let numeros = "1234567890";
 
-    for (let i = 0; i < comprimento; i++) {
-        let indice = Math.floor(Math.random() * caracteres.length);
-        senha += caracteres[indice];
-    }
-    return senha;
+  caracteres = '';
+
+  if (document.getElementById("checknum").checked) {
+    caracteres += numeros;
+  }
+  if (document.getElementById("checkmai").checked) {
+    caracteres += maiusculas;
+  }
+  if (document.getElementById("checkmin").checked) {
+    caracteres += minusculas;
+  }
+  if (document.getElementById("checksim").checked) {
+    caracteres += simbolos;
+  }
+
+
+  let senha = "";
+
+  for (let i = 0; i < comprimento; i++) {
+    let indice = Math.floor(Math.random() * caracteres.length);
+    senha += caracteres[indice];
+  }
+  return senha;
 }
